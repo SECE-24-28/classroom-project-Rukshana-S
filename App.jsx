63cd52176e8e694b8b21a2172243325118df295a
Rukshana-S <rukshana.s2024cse@sece.ac.in>
2025-12-02 22:03:38 +0530

    Day2 of MERN INTERN

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import FlexBoxComponent from "./flex-box/flex-box-component";
+import HelloWorldComponent from "./hello-world/hello-world-component";
+
+function App() {
+  return (
+    <div>
+      {/* <HelloWorldComponent /> */}
+      <FlexBoxComponent />
+    </div>
+  );
+}
+
+export default App;
