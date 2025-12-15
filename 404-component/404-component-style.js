8d3487471a276e9eb32277f38460767d66c9cef1
Rukshana-S <rukshana.s2024cse@sece.ac.in>
2025-12-15 08:55:41 +0530

    Day9 of MERN INTERN

---
+++ 404-component/404-component-style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+export const FourOFourStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  color: red;
+`;
